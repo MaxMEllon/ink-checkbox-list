a8ac0697b711cc4f59bafeb50bb0f6bb2587c75b
MaxMEllon <maxmellon1994@gmail.com>
2017-07-18 02:27:03 +0900

    :memo:

--- example/index.js
+++ example/index.js
@@ -1,40 +1,21 @@
-const { h, render } = require('ink');
-const { List, ListItem } = require('../lib');
+const {h, render} = require('ink');
+const {List, ListItem} = require('../lib');
 const readline = require('readline');
 
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 process.stdin.resume();
-process.stdin.setEncoding('utf8');
 
-const unmount = render(h(
-	List,
-	{
-		onSubmit: list => {
-			unmount();
-			console.log(list);
+const unmount = render(
+	<List
+		onSubmit={(list) => {
+			console.log(list)
 			process.exit(0);
-		}
-	},
-	h(
-		ListItem,
-		{ value: '1' },
-		'option1'
-	),
-	h(
-		ListItem,
-		{ value: '2' },
-		'option2'
-	),
-	h(
-		ListItem,
-		{ value: '3' },
-		'option3'
-	),
-	h(
-		ListItem,
-		{ value: '4' },
-		'option4'
-	)
-));
-
+		}}
+	>
+		<ListItem value="1">option1</ListItem>
+		<ListItem value="2">option2</ListItem>
+		<ListItem value="3">option3</ListItem>
+		<ListItem value="4">option4</ListItem>
+	</List>
+);
